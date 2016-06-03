944953b61690141adba7569927f0aeb1c8f14306
csnedecor <csnedecor@worthwild.com>
2016-06-03 09:51:28 -0400

    Styles 404 page

--- 404.php
+++ 404.php
@@ -1,64 +1,23 @@
-<?php
-/**
- * The template for displaying 404 pages (not found).
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package Casco_Bay_Transportation
- */
-
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'casco-bay-transportation' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'casco-bay-transportation' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( casco_bay_transportation_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'casco-bay-transportation' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'casco-bay-transportation' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+<?php get_header(); ?>
+<div class="casco_header_breadcrum_title">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1><?php _e('404 Error','snedecor'); ?></h1>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row casco_blog_wrapper">
+		<div class="col-md-12 hc_404_error_section">
+			<div class="error_404">
+				<h2><?php _e('404','snedecor'); ?></h2>
+				<h4><?php _e('Whoops... Page Not Found !!!','snedecor'); ?></h4>
+				<p><?php _e('We`re sorry, but the page you are looking for doesn`t exist.','snedecor'); ?></p>
+				<p><a href="<?php echo home_url( '/' ); ?>"><button class="casco_send_button" type="submit"><?php _e('Go To Homepage','snedecor'); ?></button></a></p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
